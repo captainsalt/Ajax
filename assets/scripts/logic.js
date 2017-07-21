0e93bab019b6a2ee573075983aa2571d1c5cc13e
David Wright <dontbothertheking@gmail.com>
2017-07-20 22:34:50 -0500

    Added request function

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,7 +1,15 @@
 ﻿$(document).ready(() => {
 
+    $(document).click(() => {
+        console.log(request("Elephant"));
+    });
 
+    function request(search) {
+        var key = "0964cff7104b440d886afa3fe6ec8dcd";
+        var baseUri = "https://api.giphy.com";
+        var resource = `/v1/gifs/search?api_key=${key}&q=${search}`;
 
-
-
+        var response = $.get(`${baseUri}${resource}`);
+        return response;
+    }
 });
