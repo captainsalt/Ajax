a6a99400527b989690e1819c06bfff9bca114278
David Wright <dontbothertheking@gmail.com>
2017-07-21 04:38:40 -0500

    Images populate div and gif stat/stop when you click one

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,15 +1,48 @@
 ﻿$(document).ready(() => {
+    populateImageArea("tiger"); //test code
 
-    $(document).click(() => {
-        console.log(request("Elephant"));
-    });
+    async function populateImageArea(searchQuery) {
+        var imageDiv = $("#imageContainer");
+        var response = await request(searchQuery);
 
-    function request(search) {
-        var key = "0964cff7104b440d886afa3fe6ec8dcd";
-        var baseUri = "https://api.giphy.com";
-        var resource = `/v1/gifs/search?api_key=${key}&q=${search}`;
+        //clear image div
+        $("#imageContainer").empty();
 
-        var response = $.get(`${baseUri}${resource}`);
-        return response;
+        for (var i = 0; i < response.data.length; i++) {
+            var gif = $(`<img>`)
+                .addClass("image")
+                .data("data-is_active", "")
+                .data("data-inactive", response.data[i].images.fixed_width_small_still.url)
+                .data("data-active", response.data[i].images.fixed_width_small.url)
+                .attr("src", response.data[i].images.fixed_width_small_still.url);
+
+            imageDiv.append(gif);
+        }
+
+        hookIntoImageEvents();
+
+        //helper functions
+        function request(searchQuery, limit = 24) {
+            var key = "0964cff7104b440d886afa3fe6ec8dcd";
+            var baseUri = "https://api.giphy.com";
+            var resource = `/v1/gifs/search?api_key=${key}&q=${searchQuery}&limit=${limit}`;
+
+            var response = $.get(`${baseUri}${resource}`);
+            return response;
+        }
+
+        function hookIntoImageEvents() {
+            $(".image").click(e => {
+                var image = $(e.currentTarget);
+
+                if (image.data("data-is_active")) {
+                    image.attr("src", image.data("data-inactive"))
+                        .data("data-is_active", false);
+                } else {
+                    image.attr("src", image.data("data-active"))
+                        .data("data-is_active", true);
+                }
+            });
+        }
     }
 });
